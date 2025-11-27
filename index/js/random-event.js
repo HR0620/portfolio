779cae4ce61e0eeecd64876a32c591a4ef5a3adb
HR0620 <195055595+HaradaRenju2008@users.noreply.github.com>
2025-11-27 12:08:27 +0900

    add rondom-event

---
+++ index/js/random-event.js
@@ -0,0 +1,19 @@
+/* Undertaleのようにランダムでイベントが発生する機能を実装するためのJavaScriptコード
+FUN値(1-100)をサイト訪問時に与えてそれに基づいてランダムイベントを発生させる
+*/
+//ダークモードボタンの三日月を満月に変える
+//ハンバーガーメニューのボタンの見た目をハンバーガーに変える
+//高専祭サムネをhisayoshi_thumbnail-color.pngに変える
+
+
+
+
+
+
+
+
+
+
+
+
+console.log("Oh...! You found me! So embarrassing.");
